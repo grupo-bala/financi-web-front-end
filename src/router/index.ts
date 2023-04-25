e683f1b1fb8cdd2f39b066d54447d3a69a40cdf1
Henok Teixeira <henokteixeira@gmail.com>
2023-04-25 13:04:38 -0300

    feat: config vue-routes to use multiple pages

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {path: '/', name: 'Home', component: Home}
+  /** 
+   * para adicionar novas rotas para outras páginas, faça isso de forma dinâmica 
+   * ex:
+   * {path: '/exemplo', name: 'exemplo', component: () => import('path-da-view')}
+  */
+]
+
+const router = createRouter({
+  history: createWebHistory("/financi-web-front-end/"),
+  routes
+})
+
+export default router;
