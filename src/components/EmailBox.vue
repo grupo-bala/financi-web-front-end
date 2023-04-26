22122df937e7bd2eb6c1103e4d1697f25e109496
MLeandr0 <matheusleandro33@gmail.com>
2023-04-26 10:55:48 -0300

    Style: email_password_components

---
+++ src/components/EmailBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="email">
+        <input type="text" id="email_login" name="email" placeholder="  insira o seu email">
+    </div>
+</template>
+
+<style scoped>
+#email_login{
+  width: 328px;
+  height: 72px;
+  border-radius: 5px;
+  border-color: gray;
+  background: transparent;
+  box-shadow: none;
+}
+</style>
---
+++ src/components/PasswordBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="password">
+        <input type="text" id="password_login" name="password" placeholder="  *****************">
+    </div>
+</template>
+
+<style scoped>
+#password_login{
+  width: 328px;
+  height: 72px;
+  border-radius: 5px;
+  border-color: gray;
+  background: transparent;
+  box-shadow: none;
+}
+</style>
