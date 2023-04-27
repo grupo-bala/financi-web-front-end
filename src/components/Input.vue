d780c025bab8c7a3ba426e3a848b64a51b12ceec
Henok Teixeira <henokteixeira@gmail.com>
2023-04-27 20:20:55 -0300

    feat: add input component

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  type: String,
+  placeholder: String,
+  modelValue: String
+});
+defineEmits(["update:modelValue"]);
+
+</script>
+
+<template>
+  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :type="type" :placeholder="placeholder">
+</template>
+
+<style scoped lang="scss">
+@import "../colors.scss";
+  input {
+    background: transparent;
+    width: 328px;
+    padding: 20px;
+    border: 1px solid gray;
+    border-radius: 5px;
+    outline: none;
+    color:  $text-color-white;
+    font-size: 1em;
+  }
+</style>
