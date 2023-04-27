8897dcf6e2876fb6c3129b01a0d330f7bc457dec
Henok Teixeira <henokteixeira@gmail.com>
2023-04-27 20:18:16 -0300

    feat: add register route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  {path: '/', name: 'Home', component: Home}
+  {path: '/', name: 'Home', component: Home},
+  {path: '/login', name: 'Login', component: () => import('../views/Login.vue')},
+  {path: '/register', name: 'Register', component: () => import('../views/Register.vue')},
   /** 
    * para adicionar novas rotas para outras páginas, faça isso de forma dinâmica 
    * ex:
