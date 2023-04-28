5241ca341e24964c12dc9a3dcc7b463decede06a
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-04-27 23:47:57 -0300

    refactor: change navbar folde

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import Navbar from './components/mobile/navbarMobile.vue';
 </script>
 
 <template>
   <router-view></router-view>
-  <Navbar/>
 </template>
 
 <style scoped>
