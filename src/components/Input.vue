0705edac8966e8167fb780b70b3816b349345eff
Henok Teixeira <henokteixeira@gmail.com>
2023-04-29 17:23:26 -0300

    feat: add regex concerning input type

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,27 +1,52 @@
 <script setup lang="ts">
-defineProps({
-  type: String,
-  placeholder: String,
-  modelValue: String
-});
-defineEmits(["update:modelValue"]);
-
+const {type, placeholder, modelValue} = defineProps<{
+  type: "Email" | "Password" | "Text",
+  label: string,
+  placeholder: string,
+  modelValue: string
+}>();
+defineEmits(["update:modelValue", "isCorrect"]);
+const regex = {
+  Email: /^[\w\-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  Password: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).+$/,
+  Text: /.?/,
+};
 </script>
 
 <template>
-  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :type="type" :placeholder="placeholder">
+  <div class="container">
+    <label class="container__label" :to="type">{{ label }}</label>
+    <input class="container__input" :id="type" :value="modelValue" @input="[$emit('update:modelValue', $event.target.value), $emit('isCorrect', regex[type].test($event.target.value))]" :type="type" :placeholder="placeholder">
+  </div>
 </template>
 
 <style scoped lang="scss">
-@import "../colors.scss";
-  input {
-    background: transparent;
-    width: 328px;
-    padding: 20px;
-    border: 1px solid gray;
-    border-radius: 5px;
-    outline: none;
-    color:  $text-color-white;
-    font-size: 1em;
+  @import "../variables.scss";
+
+  .container {
+
+    &__label {
+      color: $text-color-white;
+      font-size: 1.1em;
+      font-weight: 500;
+      &::after {
+        content: '*';
+        color: $financi-red;
+        margin-left: 2px;
+      }
+    }
+
+    &__input {
+      margin-top: 10px;
+      background: transparent;
+      width: 100%;
+      padding: 20px;
+      border: 1px solid gray;
+      border-radius: $border-radius;
+      outline: none;
+      color:  $text-color-white;
+      font-size: 1em;
+    }
+
   }
 </style>
