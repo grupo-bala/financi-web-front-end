7679fef1ef011e2a047d14a134cfc31b2a0e39c1
MLeandr0 <matheusleandro33@gmail.com>
2023-04-30 17:00:19 -0300

    style: creating a proper login checkbox component

---
+++ src/components/CheckboxLogin.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const {text} = defineProps<{
+  text: string,
+}>();
+</script>
+
+<template>
+    <div class="container">
+        <input class="container__checkbox" type="checkbox" id="login__check__box" name="login_check" checked>
+        <label class="container__label" for="login__check__box" style="color: white">{{text}}</label>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+
+    .container {
+        
+        &__checkbox {
+            width: 1.2em;
+            height: 1.2em;
+            background-color: $bg-color;
+            border-radius: 100%;
+            vertical-align: middle;
+            border: 1px solid $financi-green;
+            appearance: none;
+            cursor: pointer;
+            margin-right: 8px; 
+
+            &:checked::before {
+                border-radius: 100%;
+                content: ''; 
+                position: absolute;
+                vertical-align: middle;
+                background: $financi-green; 
+                appearance: none;
+                width: 0.6em; 
+                height: 0.6em;
+                margin-top: 3px;
+                margin-left: 3px;
+            }
+        }
+    }
+</style>
