8140cd4cbdb99867afd57649513153260ea344ad
MLeandr0 <matheusleandro33@gmail.com>
2023-04-29 21:47:09 -0300

    Style: changing_css_style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,48 +11,35 @@ const isEmailCorrect = ref(false);
 </script>
 
 <template>
-    <div class="Login">
-        <header>
+    <div class="main_content">
+        <header class="main_content__container__header">
             <img src="../assets/Exchange.png" alt="Logo Symbol">
-            <h1>Financi</h1>
+            <h1 class="main_content__container__header__title">Financi</h1>
         </header>
 
-        <form>
+        <form class="main_content__container__inputs">
             <Input label="Email" type="Email" placeholder="Digite sua e-mail" v-model="email" @isCorrect="(correct) => isEmailCorrect = correct"/>
             <Input label="Senha" type="Password" placeholder="Digite sua senha" v-model="password" @isCorrect="(correct) => isPasswordCorrect = correct"/>
-            <div class="keepLoged">
+            <div class="main_content__container__inputs__keep_loged">
                 <input type="checkbox" id="login__check__box" name="login_check" checked>
                 <label for="login__check__box" style="color: white">Mantenha-me conectado</label>
-                <p class="main_content__container__inputs__error" v-if="email.length === 0">E-mail não preenchido</p>
-                <p class="main_content__container__inputs__error" v-else-if="!isEmailCorrect">E-mail no formato errado</p>
-                <p class="main_content__container__inputs__error" v-else-if="password.length === 0">Senha não preenchida</p>
+                <p class="main_content__container__inputs__keep_loged__error" v-if="email.length === 0">E-mail não preenchido</p>
+                <p class="main_content__container__inputs__keep_loged__error" v-else-if="!isEmailCorrect">E-mail no formato errado</p>
+                <p class="main_content__container__inputs__keep_loged__error" v-else-if="password.length === 0">Senha não preenchida</p>
             </div>    
         </form>
 
-        <footer>
+        <footer class="main_content__container__footer">
             <Button text="ENTRAR" :disabled= "!isPasswordCorrect || !isEmailCorrect"/>
-            <p style="color: white">Não possui uma conta? <a href = "Register">Cadastre-se aqui.</a></p>
+            <p>Não possui uma conta? <a href = "Register">Cadastre-se aqui.</a></p>
         </footer>
     </div>
 </template>
 
 
 <style scoped lang="scss">
-@import "../variables.scss";
-
-    header {
-        display:flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
-
-    h1 {
-        font-size: 48px;
-        color:  $text-color-white;
-    }
-
-    .Login {
+    @import "../variables.scss";
+    .main_content {
         display:flex;
         align-items: center;
         justify-content: center;
@@ -61,58 +48,72 @@ const isEmailCorrect = ref(false);
         height: 100vh;
         gap: 100px;
         background-color: $bg-color;
+
+        &__container__header {
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+
+            &__title {
+                font-size: 48px;
+                color:  $text-color-white;  
+            }
+        }
+
+        &__container__inputs {
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+
+            &__keep_loged {
+
+                #login__check__box {
+                    width: 1.2em;
+                    height: 1.2em;
+                    background-color: $bg-color;
+                    border-radius: 100%;
+                    vertical-align: middle;
+                    border: 1px solid $financi-green;
+                    appearance: none;
+                    cursor: pointer;
+                    margin-right: 8px; 
+                }
+
+                #login__check__box:checked::before {
+                    border-radius: 100%;
+                    content: ''; 
+                    position: absolute;
+                    vertical-align: middle;
+                    background: $financi-green; 
+                    appearance: none;
+                    width: 0.7em; 
+                    height: 0.7em;
+                    margin-top: 2px;
+                    margin-left: 2.5px;
+                }
+                
+                &__error {
+                    margin-top: 20px;
+                    color: $financi-red;
+                    font-weight: 500;
+                    text-align: left;
+                    margin-bottom: 10px;
+                }
+            }
+        }
+
+        &__container__footer {
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            gap: 25px;
+        }
     }
 
-    .main_content__container__inputs__error {
-        margin-top: 20px;
-        color: $financi-red;
-        font-weight: 500;
-        text-align: left;
-        margin-bottom: 10px;
-    }
-
-    button {
-        width: 50px;
-        height: 50px;
-    }
-
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-    }
-
-    #login__check__box {
-        width: 1.2em;
-        height: 1.2em;
-        background-color: $bg-color;
-        border-radius: 100%;
-        vertical-align: middle;
-        border: 1px solid $financi-green;
-        appearance: none;
-        cursor: pointer;
-        margin-right: 8px; 
-    }
-
-    #login__check__box:checked::before {
-        border-radius: 100%;
-        content: ''; 
-        position: absolute;
-        vertical-align: middle;
-        background: $financi-green; 
-        appearance: none;
-        width: 0.7em; 
-        height: 0.7em;
-        margin-top: 2px;
-        margin-left: 2.5px;
-    }
-
-    footer {
-        display:flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        gap: 25px;
+    p {
+        color: $text-color-white;
     }
 
     a {
