80ea4b67a99d401c39cf70793cb561deb3d8e6d0
MLeandr0 <matheusleandro33@gmail.com>
2023-05-02 01:24:54 -0300

    feat: get request in the login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 
 import { ref } from 'vue';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import Input from '../components/Input.vue';
 import Button from "../components/Button.vue";
 import CheckboxLogin from '../components/CheckboxLogin.vue';
+import router from '../router';
 
-const email = ref("");
+const username = ref("");
 const password = ref("");
+const feedback = ref("");
 const isPasswordCorrect = ref(false);
-const isEmailCorrect = ref(false);
+const isUsernameCorrect = ref(false);
+type Response = {msg: string};
+
+async function login() {
+
+    try {
+        const response = await axios.get(`https://financi.fly.dev/login?username=${username.value}&password=${password.value}`);
+        feedback.value = "";
+        router.push({name:"Home"});
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        feedback.value = (axiosError.response?.data as Response).msg;
+    }
+}
 </script>
 
 <template>
@@ -19,15 +35,15 @@ const isEmailCorrect = ref(false);
         </div>
 
         <form class="main_container__inputs">
-            <Input :numeric="false" :required="false" label="Email" type="Email" placeholder="Digite sua e-mail" v-model="email" @isCorrect="(correct) => isEmailCorrect = correct"/>
+            <Input :numeric="false" :required="false" label="Email" type="Email" placeholder="Digite seu nome de usuário" v-model="username" @isCorrect="(correct) => isUsernameCorrect = correct"/>
             <Input :numeric="false" :required="false" label="Senha" type="Password" placeholder="Digite sua senha" v-model="password" @isCorrect="(correct) => isPasswordCorrect = correct"/>
-            <div class="main_container__inputs__texts">
-                <CheckboxLogin text="Mantenha-me conectado"></CheckboxLogin>
-            </div>    
+            <CheckboxLogin text="Mantenha-me conectado"></CheckboxLogin>
+            <p class="main_container__inputs__error" v-if="feedback.length > 0">{{ feedback }}</p>
+            <p class="main_container__inputs__error--placeholder" v-else>#</p>
         </form>
 
         <div class="main_container__enter">
-            <Button text="ENTRAR" :disabled= "password.length === 0 || email.length === 0"/>
+            <Button text="ENTRAR" :disabled= "password.length === 0 || username.length === 0" @click = "login" />
             <p class="main_container__enter__text">Não possui uma conta? <router-link class="main_container__enter__link" to="/register">Cadastre-se aqui.</router-link></p>
         </div>
     </div>
@@ -62,21 +78,18 @@ const isEmailCorrect = ref(false);
             display: flex;
             flex-direction: column;
             gap: 15px;
-
-            &__texts {
                 
-                &__error {
-                    margin-top: 20px;
-                    color: $financi-red;
-                    font-weight: 500;
-                    text-align: left;
-                    margin-bottom: 10px;
+            &__error {
+                margin-top: 20px;
+                color: $financi-red;
+                font-weight: 500;
+                text-align: left;
+                margin-bottom: 10px;
 
-                    &--placeholder {
-                        margin-top: 20px;
-                        visibility: hidden;
-                        margin-bottom: 10px;
-                    }
+                &--placeholder {
+                    margin-top: 20px;
+                    visibility: hidden;
+                    margin-bottom: 10px;
                 }
             }
         }
