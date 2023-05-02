b53643c7fd0be0997a1b37a4b9be691de58d44a2
Henok Teixeira <henokteixeira@gmail.com>
2023-05-02 02:30:58 -0300

    feat: add navbar to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+  import navbarMobile from '../components/navbar/navbarMobile.vue';
+  import navbarDesktop from '../components/navbar/navbarDesktop.vue';
+  import { useMediaQuery } from '@vueuse/core';
+
+  const isDesktop = useMediaQuery('(min-width: 1281px)');
 </script>
 
 <template>
   <div class="main_content">
-    <h1 class="main_content__title">Home</h1>
+    <div class="main_content__desktop" v-if="isDesktop">
+      <nav class="main_content__desktop__nav">
+        <navbarDesktop/>
+      </nav>
+      <h1 class="main_content__title">Home</h1>
+    </div>
+    <div class="main_content__mobile" v-else>
+      <h1 class="main_content__title">Home</h1>
+      <nav class="main_content__mobile__nav">
+        <navbarMobile/>
+      </nav>
+    </div> 
+    
   </div>
 </template>
 
@@ -20,6 +37,30 @@
 
     &__title {
       color: $text-color-white;
+    }
+
+    &__desktop {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+
+      &__nav {
+        width: 100vw;
+        padding-bottom: 2rem;
+      }
+    }
+
+    &__mobile {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100vw;
+
+      &__nav {
+        position: fixed;
+        bottom: 2em;
+      }
     }
   }
 
