9f8aa01f2ed73ebb9d8bda463bb12fdf21788289
Henok Teixeira <henokteixeira@gmail.com>
2023-05-02 01:35:13 -0300

    style: add basic style to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div class="Home">
-    <h1>Teste</h1>
+  <div class="main_content">
+    <h1 class="main_content__title">Home</h1>
   </div>
 </template>
+
+<style scoped lang="scss">
+  @import "../variables.scss";
+
+  .main_content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 100svh;
+    background-color: $bg-color;
+
+    &__title {
+      color: $text-color-white;
+    }
+  }
+
+</style>
