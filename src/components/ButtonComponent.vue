0156ab55018b35b582c8af3652e46a6a0c79dfce
MLeandr0 <matheusleandro33@gmail.com>
2023-05-05 23:42:41 -0300

    refactor: change the button component code to fix eslint errors

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  text: string,
+  disabled: boolean,
+}>();
+
+</script>
+<template>
+  <div class="container">
+    <button
+      class="container__button"
+      :disabled="disabled"
+    >
+      {{ props.text }}
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+
+  .container {
+
+    &__button {
+      width: 200.35px;
+      height: 50px;
+      border: 1px solid $financi-green;
+      border-radius: 5px;
+      background-color: $financi-green;
+      color: $text-button-color;
+      font-weight: bold;
+      font-size: larger;
+      text-align: center;
+      letter-spacing: 0.08em;
+      cursor: pointer;
+
+      &:disabled {
+        cursor: not-allowed;
+        opacity: 0.6;
+      }
+    }
+  }
+</style>
