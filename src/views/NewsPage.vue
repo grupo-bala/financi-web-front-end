c9c0b37c00fed34ca4e07f5baaeb6ed603a0b7e1
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-05-14 01:16:45 -0300

    refactor: change news page to scss patern and change news page style

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -12,16 +12,17 @@ interface NewsPreview {
 
 const news = ref<NewsPreview[]>([]);
 const howManyPages = ref<number>();
-const initialPage :number = 1;
+const page :number = 1;
+//const currentPage = ref(page);
 
 async function getNews() {
   const response =
     await axios
-      .get(`https://financi.fly.dev/get-all-news-preview?page=
-      ${initialPage}&size=10`);
+      .get(`http://localhost:8080/get-all-news-preview?page=${page}&size=5`);
   const json = await response.data;
   howManyPages.value = json.pages;
   news.value = json.data;
+
 }
 
 getNews();
@@ -46,11 +47,69 @@ getNews();
           <input
             type="search"
             placeholder="Filtrar"
+            style="color: white"
           >
         </div>
+        <div v-if="news.length !== 0">
+          <li
+            v-for="{imgURL, publishDate, title } in news"
+            :key="title"
+            class="container__main__news__list"
+          >
+            <button class="container__main__news__list__button">
+              <img
+                :src="imgURL"
+                alt="Imagem da notícia"
+                class="container__main__news__list__button__image"
+              >
+              <div class="container__main__news__list__button__info">
+                <p class="container__main__news__list__button__info__title">
+                  {{ title }}
+                </p>
+                <p class="container__main__news__list__button__info__date">
+                  {{ new Date(publishDate).toLocaleDateString("pt-BR") }}
+                </p>
+              </div>
+            </button>
+          </li>
+          <button class="container__main__news__more">
+            <h4>VER MAIS</h4>
+          </button>
+        </div>
+        <div
+          v-else
+          class="container__main__news__empty"
+        >
+          <h5> Ainda não há notícias...</h5>
+        </div>
       </section>
+      <div class="container__main__fixed">
+        <aside class="container__main__fixed__aside">
+          <h5>Recomendados</h5>
+          <section class="container__main__fixed__aside__section">
+            <li
+              v-for="{imgURL, title } in news"
+              :key="title"
+            >
+              <button class="container__main__fixed__aside__section__button">
+                <img
+                  :src="imgURL"
+                  alt="Imagem da notícia"
+                  class="container__main__fixed__aside__section__button__image"
+                >
+                <div
+                  class="container__main__fixed__aside__section__button__info"
+                >
+                  <p>
+                    {{ title }}
+                  </p>
+                </div>
+              </button>
+            </li>
+          </section>
+        </aside>
+      </div>
     </main>
-    <aside />
     <footer>
       <Logo />
     </footer>
@@ -62,15 +121,18 @@ getNews();
 
 .container {
   background-color: $bg-color;
-  height: 100vh;
+  height: auto;
+  min-height: 100vh;
 
-  @media (max-width: 600px) {
     &__main {
-        padding-top: 2rem;
+        padding: 2rem;
         display: flex;
         justify-content: start;
+        align-items: start;
+        height: auto;
+        overflow: hidden;
 
-      &__news {
+        &__news {
         background-color: $bg-color;
         display: flex;
         justify-content: center;
@@ -79,44 +141,147 @@ getNews();
         font-weight: bold;
         border-radius: $border-radius;
         font-size: 1.3rem;
+        flex-grow: 1;
+        list-style: none;
+
         &__filter {
           display: flex;
           align-items: center;
           background-color: $filter-bg-color;
           border-radius: $border-radius;
           padding-left: 1rem;
-          box-shadow: $box-shadow;
+          box-shadow: $filter-box-shadow;
+          margin: 1.5rem 0;
+        }
+        &__more {
+          width: 100%;
+          height: 2rem;
+          border-radius: $border-radius;
+          text-align: center;
+          flex-grow: 1;
+          background-color: $financi-green;
+          color: $text-color-white;
+          margin-top: 1rem;
+        }
+        &__list {
+          padding: 0.5rem 0;
+
+          &__button {
+            display: flex;
+            width: 100%;
+            height: 5rem;
+            align-items: center;
+            border-radius: $border-radius;
+            box-shadow: $filter-box-shadow;
+            background-color: $section-color;
+            color: $text-color-white;
+            &__image {
+              width: 5rem;
+              padding: 0.7rem;
+              border-radius: 20px;
+              object-fit: contain;
+            }
+
+            &__info {
+              flex-grow: 1;
+              display: flex;
+              padding-right: 1.5rem;
+              justify-content: space-between;
+              align-items: center;
+              font-weight: 500;
+              text-align: left;
+
+              &__date {
+                font-size: 0.8rem;
+              }
+
+              &__title {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                font-size: 1rem;
+                max-height: 4rem;
+                max-width: 70%;
+                overflow: hidden;
+                text-overflow: ellipsis;
+              }
+            }
+          }
+        }
+        &__empty {
+          align-items: center;
         }
       }
-    }
+  }
 
-    }
-    @media (min-width: 601px) {
-    &__main {
-      padding: 3rem;
-      &__news {
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      padding: 2rem;
-      background-color: $section-color;
-      color: $text-color-white;
-      font-weight: bold;
-      border-radius: $border-radius;
-      &__title {
-        padding-top: 1rem;
-      }
+@media (max-width: 900px)  {
+  footer, aside {
+  display: none;
+  }
+}
 
-      &__filter {
-        display: flex;
-        align-items: center;
-        background-color: $filter-bg-color;
-        border-radius: $border-radius;
-        padding-left: 1rem;
-        box-shadow: $box-shadow;
+  @media (min-width: 901px) {
+  &__main {
+    padding: 3rem;
+    &__news {
+        background-color: $section-color;
+        padding: 2rem;
+        flex-grow: 0;
+        width: 65%;
+        &__list {
+            border-bottom: $border-color;
+          &__button {
+            box-shadow: none;
+            height: 7rem;
+            &__info__title {
+              padding-left: 1rem
+            }
+            &__image {
+              padding: 0;
+              border-radius: 10px;
+              max-height: 5rem;
+            }
+          }
+        }
       }
+      &__fixed {
+        &__aside {
+          position: fixed;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          background-color: $section-color;
+          margin-left: 2rem;
+          color: $text-color-white;
+          padding: 2rem 1.5rem 1.5rem 1.5rem;
+          border-radius: $border-radius;
+          list-style: none;
+          max-width: 30%;
+          &__section {
+            padding-top: 1rem;
+            &__button {
+              display: flex;
+              background-color: transparent;
+              border-top: $border-color;
+              color: $text-color-white;
+              &__image {
+                width: 5rem;
+                padding: 0.7rem;
+                border-radius: 20px;
+                object-fit: contain;
+              }
+              &__info{
+                align-self: center;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                overflow: hidden;
+                text-overflow: ellipsis;
+              }
+            }
+          }
+        }
       }
-
     }
   }
 }
@@ -133,6 +298,11 @@ input {
     color: $filter-text-color;
     opacity: 1;
     font-weight: 600;
+}
+
+button {
+  border: none;
+  cursor: pointer;
 }
 
 </style>
