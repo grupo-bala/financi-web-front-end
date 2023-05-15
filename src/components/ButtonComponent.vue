50b2f4f8879905e771598ae3825fce823735c38f
Henok Teixeira <henokteixeira@gmail.com>
2023-05-15 17:37:30 -0300

    style: add props to set button color in button component

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { computed } from "vue";
 
 const props = defineProps<{
+  color: "green" | "red" | "blue",
   text: string,
   disabled: boolean,
 }>();
+
+const getColor = computed(() => {
+  if (props.color === "red") {
+    return "#EF5350";
+  } else if (props.color === "blue") {
+    return "#168CC0";
+  }
+
+  return "#49AD5A";
+});
 
 </script>
 <template>
@@ -20,25 +32,33 @@ const props = defineProps<{
 <style scoped lang="scss">
 @import "../variables.scss";
 
-  .container {
+.container {
 
-    &__button {
-      width: 200.35px;
-      height: 50px;
-      border: 1px solid $financi-green;
-      border-radius: 5px;
-      background-color: $financi-green;
-      color: $text-button-color;
-      font-weight: bold;
-      font-size: larger;
-      text-align: center;
-      letter-spacing: 0.08em;
-      cursor: pointer;
+  &__button {
+    width: 200.35px;
+    height: 50px;
+    border: 1px solid v-bind(getColor);
+    border-radius: 5px;
+    background-color: v-bind(getColor);
+    color: $text-button-color;
+    font-weight: bold;
+    font-size: 0.9rem;
+    text-align: center;
+    letter-spacing: 0.08em;
+    cursor: pointer;
 
-      &:disabled {
-        cursor: not-allowed;
-        opacity: 0.6;
-      }
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.6;
     }
   }
+}
+
+@media screen and (min-width: 768px) {
+  .container {
+    &__button {
+      font-size: 1rem;
+    }
+  }
+}
 </style>
