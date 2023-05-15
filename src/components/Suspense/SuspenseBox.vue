c58e1da2e6bad12666edf0994e1e206c10732614
Antônio Hugo <hugorplobo@gmail.com>
2023-05-15 02:06:59 -0300

    feat: add loading components

---
+++ src/components/Suspense/SuspenseBox.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Skeletor } from "vue-skeletor";
+import "vue-skeletor/dist/vue-skeletor.css";
+
+const props = defineProps<{
+  isLoading: boolean,
+  loadingWidth: number | string,
+  loadingHeight: number | string,
+  circle?: boolean,
+  pill?: boolean,
+}>();
+</script>
+
+<template>
+  <div
+    :style="{
+      width: isLoading ? props.loadingWidth : 'auto',
+      height: isLoading ? props.loadingHeight : 'auto',
+    }"
+  >
+    <Skeletor
+      v-if="props.isLoading"
+      :width="props.loadingWidth"
+      :height="props.loadingHeight"
+      class="skeletor"
+    />
+    <slot
+      :style="{
+        display: isLoading ? 'none' : 'block'
+      }"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../../variables.scss";
+.skeletor {
+  border-radius: $border-radius;
+}
+</style>
---
+++ src/components/Suspense/SuspenseImage.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Skeletor } from "vue-skeletor";
+import "vue-skeletor/dist/vue-skeletor.css";
+
+const props = defineProps<{
+  src: string,
+  circle?: boolean,
+  pill?: boolean,
+  imgClass?: string,
+}>();
+
+const isImageLoading = ref(true);
+</script>
+
+<template>
+  <div>
+    <Skeletor
+      v-if="isImageLoading"
+      :circle="props.circle"
+      :pill="props.pill"
+      :class="'skeletor ' + props.imgClass"
+    />
+    <img
+      :src="props.src"
+      :style="{
+        display: isImageLoading ? 'none' : 'block',
+      }"
+      :class="props.imgClass"
+      @load="isImageLoading = false"
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../../variables.scss";
+
+.skeletor {
+  border-radius: $border-radius;
+}
+</style>
