e6f41e1cda528f9e0825b33f8f8a522c4260bade
MLeandr0 <matheusleandro33@gmail.com>
2023-05-15 23:10:56 -0300

    style: add new icons to the main file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,55 @@ import { createApp } from "vue";
 import "./style.scss";
 import App from "./App.vue";
 import router from "./router";
-import { IoSettingsSharp } from "oh-vue-icons/icons";
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import axios from "axios";
+import {
+  FaHome,
+  FaChartBar,
+  FaPlayCircle,
+  FaExchangeAlt,
+  IoFilter,
+  IoSettingsSharp,
+  MdVisibilityRound,
+  MdVisibilityoffRound,
+  OiTriangleDown,
+  OiTriangleUp,
+  FaBriefcaseMedical,
+  MdSchoolRound,
+  MdErroroutlineRound,
+  MdPaymentsRound,
+  MdFoodbankRound,
+  FaGifts,
+} from "oh-vue-icons/icons";
+
+export default {
+  components: {
+    "v-icon": OhVueIcon,
+  },
+};
+
+addIcons(
+  FaHome,
+  FaChartBar,
+  FaPlayCircle,
+  FaExchangeAlt,
+  IoFilter,
+  IoSettingsSharp,
+  MdVisibilityRound,
+  MdVisibilityoffRound,
+  OiTriangleDown,
+  OiTriangleUp,
+  FaBriefcaseMedical,
+  MdSchoolRound,
+  MdErroroutlineRound,
+  MdPaymentsRound,
+  MdFoodbankRound,
+  FaGifts,
+);
 
 axios.defaults.withCredentials = true;
 
-addIcons(IoSettingsSharp);
-
-createApp(App).component("v-icon", OhVueIcon).use(router).mount("#app");
+createApp(App)
+  .component("v-icon", OhVueIcon)
+  .use(router)
+  .mount("#app");
