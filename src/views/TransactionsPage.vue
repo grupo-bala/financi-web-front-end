9cebd025168eb5dd5b09145c71fc37104942a824
MLeandr0 <matheusleandro33@gmail.com>
2023-05-15 23:18:19 -0300

    feat: add the function to get the transactions in the transactions page

--- src/views/TransactionsPage.vue
+++ src/views/TransactionsPage.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 
-//import { ref } from "vue";
+import { ref } from "vue";
 import Transaction from "../components/TransactionComponent.vue";
 import Logo from "../components/LogoFinanci.vue";
 import Filter from "../components/FilterButton.vue";
 import TextBox from "../components/TextBox.vue";
+import axios, { AxiosError } from "axios";
+
+const envUrl = import.meta.env.VITE_API_URL!;
+const feedback = ref("");
+type Response = {msg: string};
+
+interface TransactionsPreview {
+    titleTransaction: string;
+    publishDate: string;
+    imgCategory: string;
+    valueAmount: number;
+}
+
+const transactions = ref<TransactionsPreview[]>([]);
+const howManyPages = ref<number>();
+
+async function getAllTransaction() {
+  try {
+    const response = await axios.post(
+      `${envUrl}/get-all-transactions-preview?page=1&size=4`);
+    feedback.value = "";
+    const json = await response.data;
+    howManyPages.value = json.pages;
+    transactions.value = json.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    const response = axiosError.response?.data as Response;
+    feedback.value = response.msg;
+  }
+}
+
+getAllTransaction();
 
 </script>
 
@@ -20,26 +52,93 @@ import TextBox from "../components/TextBox.vue";
         />
       </div>
       <div class="main_container__transactions_box__display">
-        <TextBox text="Hoje - 02 de julho" />
-        <Transaction
-          title="Mensalidade Estácio"
-          date="2 de Julho, 2022"
-          :value="100.00"
+        <TextBox
+          class="main_container__transactions_box__display__textBox"
+          text="Recentes"
         />
+        <li
+          v-for="{valueAmount,
+                  titleTransaction,
+                  publishDate,
+                  imgCategory} in transactions"
+          :key="titleTransaction"
+        >
+          <Transaction
+            :title="titleTransaction"
+            :date="publishDate"
+            :category="imgCategory"
+            :value="valueAmount"
+          />
+        </li>
         <Transaction
           title="Mensalidade Estácio"
           date="2 de Julho, 2022"
-          :value="200.00"
-        />
-        <Transaction
-          title="Mensalidade Estácio"
-          date="2 de Julho, 2022"
+          category="Payments"
           :value="2000.00"
         />
         <Transaction
           title="Mensalidade Estácio"
           date="2 de Julho, 2022"
+          category="Studies"
           :value="-2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Hospital"
+          :value="200.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Payments"
+          :value="2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Studies"
+          :value="-2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Hospital"
+          :value="200.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Payments"
+          :value="2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Studies"
+          :value="-2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Hospital"
+          :value="200.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Payments"
+          :value="2000.00"
+        />
+        <Transaction
+          title="Mensalidade Estácio"
+          date="2 de Julho, 2022"
+          category="Studies"
+          :value="-2000.00"
+        />
+        <TextBox
+          class="main_container__transactions_box__display__textBox"
+          text="Outros dias"
         />
       </div>
     </div>
@@ -51,34 +150,52 @@ import TextBox from "../components/TextBox.vue";
     @import "../variables.scss";
 
     .main_container {
-        display:flex;
+        display: flex;
         align-items: center;
         justify-content: space-evenly;
         flex-direction: column;
         margin: 0;
         min-height: 100svh;
-        background-color: $bg-color;
+        background-color: $bg-color; //realColor
+        //background-color: pink; //help
 
         &__transactions_box {
-            height: 600px;
-            width: 688px;
+            height: auto;
             border-radius: 15px;
+            //background-color: green; //help
+            display: flex; //novo
+            flex-direction: column;
 
             @media (min-width: 320px) {
-              background-color: $bg-color;
+            //  background-color: $bg-color; //realColor
               width: 412px;
             }
             @media (min-width: 600px) {
-              background-color: $card-bg-color;
+            //  background-color: $card-bg-color; //realColor
               width: 688px;
+            }
+            @media (min-width: 1444px) {
+            //  background-color: $card-bg-color; //realColor
+              width: 1500px;
             }
 
             &__display {
               display: flex;
               flex-direction: column;
-              justify-content: space-evenly;
+              gap: 15px;
               align-items: left;
-              height: 500px;
+              height: auto; //new height
+              //background-color: blue; //help
+              margin-bottom: 27px;
+
+              &__textBox {
+                margin-top: 83px;
+                margin-bottom: 27px;
+
+                @media (min-width: 320px) {
+                  margin-top: 27px;
+                }
+              }
 
               @media (min-width: 320px) {
                 width: 412px;
@@ -98,6 +215,8 @@ import TextBox from "../components/TextBox.vue";
                 flex-direction: column;
                 border-radius: 15px;
                 padding: 10px;
+                margin-top: 33px; //new
+                //background-color: yellow; //help
 
                 @media (max-width: 599px) {
                   align-items: center;
