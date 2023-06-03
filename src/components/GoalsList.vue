12ae1604954a7f50da6d7f8aae8e5acc9a9d778f
Henok Teixeira <henokteixeira@gmail.com>
2023-06-02 23:44:28 -0300

    feat: add partial goals list component

---
+++ src/components/GoalsList.vue
@@ -0,0 +1,292 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useGoalsStore } from "../stores/goalsStore";
+import { Goal } from "../types/Goal";
+import axios from "axios";
+import { displayDate } from "../utils/Dates";
+import SuspenseBox from "./Suspense/SuspenseBox.vue";
+import Progress from "./ProgressBarComponent.vue";
+import InfoPopup from "./Popup/InfoOperationPopup.vue";
+
+type SuccesfulReponse = {
+  data: Goal[],
+  pages: number,
+}
+
+const props = defineProps<{
+  quantity: number,
+  showLoadMore?: boolean,
+}>();
+
+const baseURL = import.meta.env.VITE_API_URL as string;
+const initialPage = 0;
+const isLoading = ref(true);
+const page = ref(initialPage);
+const totalPages = ref(initialPage);
+const goals = useGoalsStore();
+const popupIsOpen = ref(false);
+const currentGoal = ref<Goal>();
+
+const goalsList = computed(() => {
+  return props.showLoadMore
+    ? goals.data
+    : goals.get(props.quantity);
+});
+
+async function getGoals() {
+  isLoading.value = true;
+  page.value++;
+
+  if (goals.page >= page.value) {
+    isLoading.value = false;
+    return;
+  }
+
+  const res = await axios.get<SuccesfulReponse>(
+    `${baseURL}/get-all-goals?
+      page=${page.value}&
+      size=${props.quantity}
+    `.replaceAll(/^\s+|\n/gm, ""),
+  );
+
+  goals.concat(res.data.data);
+  goals.nextPage();
+
+  totalPages.value = res.data.pages;
+  isLoading.value = false;
+}
+
+function getProgressPercent(totalValue: string, currentValue: string): number {
+  const percent = 100;
+
+  return Number(currentValue) / Number(totalValue) * percent;
+}
+
+function getFormatedDate(date: string) {
+  return displayDate(date);
+}
+
+function getNumberAsCurrency(value: string) {
+  return Number(value)
+    .toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+}
+
+getGoals();
+</script>
+
+<template>
+  <div>
+    <SuspenseBox
+      :is-loading="isLoading && page === 1"
+      loading-width="100%"
+      loading-height="150px"
+      :quantity="props.quantity"
+    >
+      <ul
+        class="goals_list"
+      >
+        <li
+          v-for="goal of goalsList"
+          :key="goal.id"
+          class="goals_list__item"
+          @click="[
+            popupIsOpen = true,
+            currentGoal = goal
+          ]"
+        >
+          <div class="goals_list__item__left">
+            <p class="goals_list__item__left__title">
+              {{ goal.title }}
+            </p>
+            <div class="goals_list__item__left__content">
+              <div class="goals_list__item__left__content__label_value">
+                <div
+                  class="goals_list__item__left__content__label_value__title"
+                >
+                  <p>
+                    Objetivo
+                  </p>
+                </div>
+                <div
+                  class="goals_list__item__left__content__label_value__value"
+                >
+                  {{ "R$ " + getNumberAsCurrency(goal.totalValue) }}
+                </div>
+              </div>
+              <div class="goals_list__item__left__content__label_value">
+                <div
+                  class="goals_list__item__left__content__label_value__title"
+                >
+                  <p>
+                    Pretendo alcançar em
+                  </p>
+                </div>
+                <div
+                  class="goals_list__item__left__content__label_value__value"
+                >
+                  {{ getFormatedDate(goal.deadline) }}
+                </div>
+              </div>
+              <div class="goals_list__item__left__content__label_value">
+                <div
+                  class="goals_list__item__left__content__label_value__title"
+                >
+                  <p>
+                    Ideal por mês
+                  </p>
+                </div>
+                <div
+                  class="goals_list__item__left__content__label_value__value"
+                >
+                  R$ 10,00
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="goals_list__item__right">
+            <Progress
+              :percent="getProgressPercent(goal.totalValue, goal.currentValue)"
+            />
+            <div class="goals_list__item__right__bottom">
+              <p
+                class="goals_list__item__right__bottom__current_value"
+              >
+                {{ "R$ " + getNumberAsCurrency(goal.currentValue) }}
+              </p>
+              <p class="goals_list__item__right__bottom__remaining">
+                {{ "Faltam R$ " + getNumberAsCurrency(goal.totalValue) }}
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <InfoPopup
+        v-if="popupIsOpen"
+        type="Goal"
+        :operation="currentGoal"
+        @close="popupIsOpen = false"
+      />
+    </SuspenseBox>
+    <SuspenseBox
+      :is-loading="isLoading && page > 1"
+      loading-width="100%"
+      loading-height="150px"
+      :quantity="props.quantity"
+    />
+    <button
+      v-if="props.showLoadMore && totalPages !== page"
+      :disabled="isLoading"
+      @click="getGoals()"
+    >
+      <h4>VER MAIS</h4>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+
+.goals_list {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+
+  &__item {
+    background-color: $card-bg-color;
+    padding: 1.5rem;
+    border-radius: $border-radius;
+    box-shadow: $box-shadow;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+
+    &__left {
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      gap: .8rem;
+      flex-grow: 0.8;
+      overflow: hidden;
+
+      &__title {
+        font-weight: bold;
+        font-size: 1.1rem;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        color: $text-color-white;
+      }
+
+      &__content {
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+        font-size: .8rem;
+
+        &__label_value {
+          display: flex;
+          flex-direction: column;
+          gap: .3rem;
+
+          &__title {
+            color: $text-color-gray;
+            font-weight: 600;
+          }
+
+          &__value {
+            color: $financi-blue;
+            font-weight: 600;
+          }
+        }
+      }
+    }
+
+    &__right {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: .7rem;
+
+      &__bottom {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        font-size: .9rem;
+        gap: .2rem;
+
+        &__current_value {
+          color: $financi-blue;
+          font-weight: 700;
+        }
+
+        &__remaining {
+          text-align: center;
+          font-weight: 500;
+          color: $text-color-white;
+        }
+      }
+    }
+  }
+}
+
+button {
+  width: 100%;
+  height: 2rem;
+  border-radius: $border-radius;
+  text-align: center;
+  background-color: $financi-green;
+  color: $text-color-white;
+  margin-top: 1rem;
+  border: none;
+  cursor: pointer;
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: .2;
+  }
+}
+</style>
