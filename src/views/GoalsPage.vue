7a920327f4e68c1a47154592050f463416620a40
Henok Teixeira <henokteixeira@gmail.com>
2023-06-02 23:45:58 -0300

    feat: add goals page and set a path in url

---
+++ src/views/GoalsPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import GoalsList from "../components/GoalsList.vue";
+</script>
+
+<template>
+  <GoalsList
+    :quantity="2"
+    :show-load-more="true"
+  />
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+</style>
