cef1b2a6e2289581b8d5ca51313bfe44fd350589
MLeandr0 <matheusleandro33@gmail.com>
2023-06-05 00:26:30 -0300

    feat: create the profile page and the profile component used in the profile page

---
+++ src/components/ProfileComponent.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+//import { onMounted, ref } from "vue";
+//import { ref } from "vue";
+
+const props = defineProps<{
+  username: string
+}>();
+
+/*
+function setGreeting() {
+  const hours = {
+    hour: new Date().getHours(),
+    day: 0,
+    afternoon: 12,
+    night: 18,
+  };
+
+  if (hours.hour >= hours.day && hours.hour < hours.afternoon) {
+    greeting.value = "Bom dia!";
+  } else if (hours.hour >= hours.afternoon && hours.hour < hours.night) {
+    greeting.value = "Boa tarde!";
+  } else {
+    greeting.value = "Boa noite!";
+  }
+}
+
+function getGreetingAndsSetInterval() {
+  const interval = 900000;
+
+  setGreeting();
+
+  setInterval(setGreeting, interval);
+}
+
+onMounted(() => {
+  getGreetingAndsSetInterval();
+});*/
+</script>
+
+<!-- Atualizar para pegar o url da foto usuario nas props -->
+
+<template>
+  <div class="profile_container">
+    <div class="profile_container__user_info">
+      <img
+        src="https://picsum.photos/500"
+        alt="user"
+        class="profile_container__user_info__img"
+      >
+      <p class="profile_container__user_info__username">
+        @{{ props.username }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scooped lang="scss">
+@import "../variables.scss";
+
+.profile_container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  &__user_info {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+
+    &__img {
+      width: 132px;
+      height: 132px;
+      border: 3px solid $financi-green;
+      border-radius: 50%;
+    }
+
+    &__username {
+      color: $text-color-white;
+      font-size: 24px;
+      font-weight: 600;
+    }
+  }
+}
+</style>
---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+import InputField from "../components/Inputs/InputField.vue";
+import Profile from "../components/ProfileComponent.vue";
+import FinanciLogo from "../components/LogoFinanci.vue";
+import Button from "../components/ButtonComponent.vue";
+import { ref } from "vue";
+import axios from "axios";
+
+interface UserInfo {
+  id: number,
+  name: string,
+  username: string,
+  fixedIncome: string,
+  email: string,
+}
+
+const isNameCorrect = ref(false);
+const isEmailCorrect = ref(false);
+const info = ref<UserInfo>({
+  id: -1,
+  name: "",
+  username: "",
+  fixedIncome: "",
+  email: "",
+});
+const envUrl = import.meta.env.VITE_API_URL;
+
+async function getMe() {
+  const response = await axios.get(`${envUrl}/get-me`);
+  const json = response.data;
+  info.value = json.data;
+}
+
+async function updateProfile() {
+
+}
+
+getMe();
+</script>
+
+<template>
+  <div class="main_container">
+    <div class="main_container__content_box">
+      <Profile
+        :username="info!.username"
+        class="container__content__greetings"
+      />
+      <InputField
+        v-model="info!.name"
+        :numeric="false"
+        required
+        label="Nome"
+        type="Text"
+        placeholder="Digite seu nome de usuário"
+        @is-correct="(correct: boolean) => isNameCorrect = correct"
+      />
+      <InputField
+        v-model="info!.email"
+        :numeric="false"
+        required
+        label="E-mail"
+        type="Email"
+        placeholder="Digite seu e-mail"
+        @is-correct="(correct: boolean) => isEmailCorrect = correct"
+      />
+      <InputField
+        v-model="info!.fixedIncome"
+        numeric
+        required
+        label="Renda"
+        type="Text"
+        placeholder="Digite sua renda mensal"
+      />
+      <Button
+        class="main_container__content_box__butto-update"
+        color="green"
+        text="EDITAR PERFIL"
+        :disabled="info!.name.length === 0 || info!.email.length === 0"
+        @click="updateProfile"
+      />
+      <router-link
+        class="main_container__content_box__link"
+        to="/login"
+      >
+        FAZER LOGOUT.
+      </router-link>
+    </div>
+    <FinanciLogo class="main_container__logo-financi" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../variables.scss";
+.main_container {
+    margin: 0px;
+    display: flex;
+    height: calc(100vh - 4rem);
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    background-color: $bg-color;
+
+    &__content_box {
+        margin-top: 20px;
+        height: 750px;
+        display: flex;
+        border-radius: 15px;
+        align-items: center;
+        flex-direction: column;
+        justify-content: space-evenly;
+
+        @media (min-width: 400px) {
+          width: 400px;
+          background-color: $bg-color
+        }
+
+        @media (min-width: 800px) {
+          width: 1000px;
+          background-color: $card-bg-color;
+        }
+
+        &__link {
+          color: $financi-red;
+        }
+
+    }
+
+    &__logo-financi {
+      padding: 0px;
+      margin-bottom: 20px;
+    }
+}
+</style>
