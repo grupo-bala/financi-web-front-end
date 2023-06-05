cea2b14f5ba123599d41253ce8e77ac2504fdaec
MLeandr0 <matheusleandro33@gmail.com>
2023-06-05 00:27:09 -0300

    feat: add the profile path to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,8 +23,8 @@ const routes = [
     component: () => import("../views/CoursesPage.vue") },
   { path: "/goals", name: "Goals",
     component: () => import("../views/GoalsPage.vue") },
-  { path: "/perfil", name: "Perfil",
-    component: () => import("../views/PerfilPage.vue") },
+  { path: "/profile", name: "Profile",
+    component: () => import("../views/ProfilePage.vue") },
 ];
 
 const noAuthRoutes = [
