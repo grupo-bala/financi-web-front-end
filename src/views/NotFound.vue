caf18e91bd97cbd41db70cfc889e079187f11967
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-06-06 02:31:54 -0300

    feat: add not found image in not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,11 @@
 <template>
   <main class="not_found">
     <h1>Essa página não existe...</h1>
+    <img
+      alt="Imagem de gato confuso"
+      src="../assets/NotFoundImage.png"
+      class="not_found__image"
+    >
   </main>
 </template>
 
@@ -12,8 +17,19 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   padding: 2rem;
   background-color: $bg-color;
   color: $text-color-white;
+
+  &__image {
+    width: 20em;
+  }
+}
+
+@media (min-width: 800px){
+  .not_found{
+    flex-direction: row;
+  }
 }
 </style>
