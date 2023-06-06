d133d390dcdcf8daa9600228eb76c9a216cef371
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2023-06-06 02:40:52 -0300

    style: add style to lessons page

--- src/views/LessonsPage.vue
+++ src/views/LessonsPage.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import SuspenseBox from "../components/Suspense/SuspenseBox.vue";
 import LessonsList from "../components/LessonsList.vue";
+import Logo from "../components/LogoFinanci.vue";
+import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
-//import { defineStore } from "pinia";
 
 type Lesson = {
     name: string,
@@ -13,112 +12,117 @@ type Lesson = {
     courseId: number,
 };
 
-type LessonsReponse = {
-  data: Lesson[],
-  pages: number,
+type Course = {
+    title: string,
+    description: string,
+    id: number,
+    howManyLessons: number,
+    averageTimePerLesson: number,
+    totalTime: number,
 };
 
-// type LessonReponse = {
-//   data: Lesson,
-//   pages: number,
-// };
-// const currentLesson = ref<Lesson>();
+type SuccesfulReponse = {
+  data: Course,
+}
 
 const route = useRoute();
 const router = useRouter();
 const courseId = route.params.id;
-const baseURL = import.meta.env.VITE_API_URL as string;
-const isLoading = ref(true);
-const initialPage = 0;
-const page = ref(initialPage);
-const totalPages = ref(initialPage);
-const lessons = ref<Lesson[]>([]);
-const quantity = 10;
-const courseTitle = "Example";
 
-async function getAllLessons() {
-  isLoading.value = true;
+const currentLesson = ref<Lesson>();
+const course = ref<Course>();
+const baseURL = import.meta.env.VITE_API_URL as string;
+
+async function getCourse(){
   try {
-    page.value++;
-    const res = await axios.get<LessonsReponse>(
-      `${baseURL}/get-all-lessons?page=${page.value}&size=${quantity}
-      &courseId=${courseId}`,
+    const res = await axios.get<SuccesfulReponse>(
+      `${baseURL}/get-course?courseId=${courseId}`,
     );
     const json = res.data;
-    totalPages.value = json.pages;
-    lessons.value = json.data;
-    isLoading.value = false;
-  } catch (e) {
-    router.push("/ops");
+    course.value = json.data;
+  } catch(e){
+    //router.push("/ops");
   }
 }
 
-// async function getLesson(){
-//   try {
-//     const res = await axios.get<LessonReponse>(
-//       `${baseURL}/get-lesson?id=${}
-//       &courseId=${courseId}`,
-//     );
-//     const json = res.data;
-//     currentLesson.value = json.data;
-//   } catch (e) {
-//     router.push("/ops");
-//   }
-// }
-
-getAllLessons();
+getCourse();
 </script>
 
 <template>
-  <main>
-    <div class="container">
-      <h3> {{ courseTitle }} </h3>
-      <div class="container__video">
+  <main class="lessons">
+    <div class="lessons__container">
+      <h3> {{ course?.title }} </h3>
+      <div class="lessons__container__video">
         <iframe
-          width="100%"
-          height="315"
           src="https://www.youtube.com/embed/videoCode"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media;
           gyroscope; picture-in-picture; web-share;"
           allowfullscreen
-        >
-          .
-        </iframe>
-      </div>
-      <div class="container__list">
-        <SuspenseBox
-          :is-loading="isLoading"
-          loading-width="100%"
-          loading-height="300px"
-          :quantity="1"
         />
+      </div>
+      <div class="lessons__container__list">
         <LessonsList
           :quantity="10"
         />
       </div>
     </div>
+    <footer>
+      <Logo />
+    </footer>
   </main>
 </template>
 
 <style scoped lang="scss">
   @import "../variables.scss";
 
-.container {
-  padding-top: 2rem;
-  display: flex;
-  flex-direction: column;
+.lessons {
   width: 100vw;
-  height: 100vh;
-  align-items: center;
+  min-height: 100dvh;
   color: $text-color-white;
-  &__video {
+  background-color: $bg-color;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  &__container {
     width: 100%;
-    padding: 2rem 0 1rem 0;
+    max-width: 1400px;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    &__video {
+      width: 100%;
+      padding: 2rem 0 1rem 0;
+    }
+    &__list {
+      width: 100%;
+    }
   }
-  &__list {
-    width: 100%;
+}
+
+iframe {
+  width: 100%;
+  height: 300px;
+}
+
+@media (min-width: 900px){
+.lessons {
+  justify-content: center;
+  &__container {
+    background-color: $card-bg-color;
+    border-radius: $border-radius;
+    box-shadow: $box-shadow;
+    padding: 5rem 2rem;
+    display: flex;
+    flex-direction: row;
   }
+}
+
+  iframe {
+    height: 400px;
+    padding: 1rem;
+}
 }
 </style>
